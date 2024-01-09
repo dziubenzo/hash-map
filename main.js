dc607d06aa62b581e0d8e87db59f97acf0d7abea
dziubenzo <dziubenzo@gmail.com>
2024-01-09 17:31:53 +0100

    Build set() method
    This also covers cases of appending a new node to the tail of a linked
    list if the obtained array index already contains a node. However,
    the method does not check whether buckets need to be doubled because,
    IMHO, this requires the length() method to calculate the current load
    factor and the values() method to actually build a new array of double
    size.

--- main.js
+++ main.js
@@ -3,16 +3,59 @@ class HashMap {
   #loadFactor = 0.75;
 
   constructor() {
-    this.buckets = [];
+    this.buckets = new Array(this.#capacity);
+  }
+
+  #getIndex(key) {
+    return key % this.#capacity;
+  }
+
+  #checkLimitation(index) {
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error('Trying to access index out of bound');
+    }
   }
 
   hash(value) {
-    let code = 0;
+    let key = 0;
     const primeNumber = 31;
     for (let i = 0; i < value.length; i++) {
-      code = primeNumber * code + value.charCodeAt(i);
+      key = primeNumber * key + value.charCodeAt(i);
     }
-    return code;
+    return key;
+  }
+
+  set(key, value) {
+    // Insert at the end of the linked list or update existing node
+    const insertOrUpdateNode = (node, newNode) => {
+      while (node.next !== null) {
+        if (node.key === newNode.key) {
+          (node.value = newNode), value;
+          return;
+        }
+        node = node.next;
+      }
+      node.next = newNode;
+    };
+
+    /* 
+    TO DO: Check for whether the buckets need to be resized once length() and values() are implemented
+    */
+
+    const index = this.#getIndex(key);
+    this.#checkLimitation(index);
+    const node = new Node(key, value);
+    // Insert node immediately if index empty
+    if (this.buckets[index] === undefined) {
+      return (this.buckets[index] = node);
+    }
+    let firstNode = this.buckets[index];
+    // Update value if keys match
+    if (firstNode.key === node.key) {
+      firstNode.value = node.value;
+      return;
+    }
+    insertOrUpdateNode(firstNode, node);
   }
 }
 
@@ -25,4 +68,10 @@ class Node {
 }
 
 const hashMap = new HashMap();
+const value1 = 'Fragile';
+const key1 = hashMap.hash(value1);
+hashMap.set(key1, value1);
+const value2 = 'Fragile 2';
+const key2 = hashMap.hash(value2);
+hashMap.set(key2, value2);
 console.log(hashMap.buckets);
